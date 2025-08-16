6da5218992ea7d4cf994c48e0b5532f7969aa0b8
YashdeepN <yashdeepneema@hotmail.com>
2025-08-16 00:54:08 -0500

    setup tanstack query QueryClient context

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,18 @@
-import { HeadContent, Outlet, createRootRoute } from "@tanstack/react-router";
+import {
+  HeadContent,
+  Outlet,
+  createRootRoute,
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanstackDevtools } from "@tanstack/react-devtools";
+import type { QueryClient } from "@tanstack/react-query";
 
-export const Route = createRootRoute({
+type RouterContext = {
+  queryClient: QueryClient;
+};
+
+export const Route = createRootRouteWithContext<RouterContext>()({
   head: () => ({
     meta: [
       {
