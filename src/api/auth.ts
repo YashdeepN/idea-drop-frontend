a4ea9ef9571859a6beba033451ac3538ecaa7f7d
YashdeepN <yashdeepneema@hotmail.com>
2025-08-22 04:02:34 -0500

    auth: implement logout user functionallity

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -35,3 +35,13 @@ export const loginUser = async (credentials: loginUserType) => {
     throw new Error(message);
   }
 };
+
+export const logoutUser = async () => {
+  try {
+    await api.post("/auth/logout");
+  } catch (err: any) {
+    const message = err.response?.data?.message || "Failed to logout";
+
+    throw new Error(message);
+  }
+};
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
+import { logoutUser } from "@/api/auth";
 import { useAuth } from "@/context/AuthContext";
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import { Lightbulb } from "lucide-react";
 
 const Header = () => {
-  const { user } = useAuth();
+  const navigate = useNavigate();
+  const { user, setUser, setAccessToken } = useAuth();
+
+  const handleLogout = async () => {
+    try {
+      await logoutUser();
+      setAccessToken(null);
+      setUser(null);
+      navigate({ to: "/" });
+    } catch (err) {
+      console.log("Logout failed: ", err);
+    }
+  };
 
   return (
     <header className="bg-white shadow">
@@ -53,7 +66,10 @@ const Header = () => {
               <span className="text-gray-700 font-medium px-2">
                 Welcome, {user.name}
               </span>
-              <button className="text-red-600 hover:text-red-900 font-medium transition px-3 py-2 leading-none cursor-pointer">
+              <button
+                onClick={handleLogout}
+                className="text-red-600 hover:text-red-900 font-medium transition px-3 py-2 leading-none cursor-pointer"
+              >
                 Logout
               </button>
             </>
